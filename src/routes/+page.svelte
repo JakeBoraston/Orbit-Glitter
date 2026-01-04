6a56e9fea857f80d98e8ca40c0c862acc412f92a
jakeboraston <jboraston@hotmail.co.uk>
2026-01-04 14:37:38 +0000

    walker class created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,70 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let canvas: HTMLCanvasElement;
-	let ctx: CanvasRenderingContext2D | null;
-
 	let windowWidth = $state<number>();
 	let windowHeight = $state<number>();
 
-	let width = $derived(windowWidth*0.8)
-	let height = $derived(windowHeight*0.8)
+	let width = $derived(windowWidth * 0.8);
+	let height = $derived(windowHeight * 0.8);
+
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null;
 
 	onMount(() => {
+		//get window dimensions
 		windowWidth = window.innerWidth;
 		windowHeight = window.innerHeight;
+
+		//set context api
 		const context = canvas.getContext('2d');
 		ctx = context;
-		ctx.fillStyle = 'white';
-		ctx.fillRect(1, 1, 100, 100);
+
+		let lastTime = performance.now();
+
+		// svelte-ignore perf_avoid_nested_class
+		class Walker {
+			x: number;
+			y: number;
+			xv: number;
+			xy: number;
+			constructor() {
+				this.x = width / 2;
+				this.y = height / 2;
+				this.xv = 100;
+				this.xy = 80;
+			}
+			show(delta:number) {
+				ctx.fillStyle = 'white';
+				ctx.fillRect(this.x, this.y, 2, 2);
+				walker.x = walker.x + walker.xv * delta;
+			}
+		}
+
+		let walker = new Walker();
+
+		function draw(time: number) {
+			const delta = (time - lastTime) / 1000;
+			lastTime = time;
+
+			walker.show(delta);
+
+			requestAnimationFrame(draw);
+		}
+
+		requestAnimationFrame(draw);
+	});
+
+	$effect(() => {
+		if (!canvas) return;
+
+		canvas.width = width;
+		canvas.height = height;
 	});
 </script>
+
 <svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} />
 
-<canvas bind:this={canvas} id="canvas1" width={width} height={height}></canvas>
+<canvas bind:this={canvas} id="canvas1" {width} {height}></canvas>
 
 <style>
 	canvas {
