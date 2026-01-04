a72196ce2df82c092375a7be37531273ce5b98d0
jakeboraston <jboraston@hotmail.co.uk>
2026-01-04 12:34:02 +0000

    added center main tag with glowing border

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<main class="">
-	<h1>Hello world!</h1>
+<script lang="ts">
+	let height = $state();
+	let width = $state();
+</script>
+
+<main bind:clientHeight={height} bind:clientWidth={width}>
+
 </main>
 
-<style></style>
+<style>
+	main{
+		height: 85%;
+		width: 85%;
+		background-color: #010101;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-radius: 4px;
+		box-shadow: hsl(240, 30%, 10%) 0 0 50px;
+	}
+</style>
