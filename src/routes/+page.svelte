707ab6acaa605c6021ac1a8d12a9d7160ca988e8
jakeboraston <jboraston@hotmail.co.uk>
2026-01-04 21:57:51 +0000

    created a cool circular glitter effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,74 +7,72 @@
 	let width = $derived(windowWidth * 0.8);
 	let height = $derived(windowHeight * 0.8);
 
-	const numberOfWalkers = 10;
+	const numberOfWalkers = 100;
 
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D | null;
 
 	class Walker {
-			x: number;
-			y: number;
-			r: number;
-			velocity: number;
-			size: number;
-			hue: number;
-			color: string;
-			constructor(size:number) {
-				this.size = size
-				this.r = Math.random();
-				this.x = Math.random() * width - this.size;
-				this.y = Math.random() * height - this.size;
-				this.velocity = 50;
-				this.hue = this.r * 360;
-				this.color = `hsl${this.hue},90%,50%`;
-			}
-			show(delta: number) {
-				const randNum = Math.floor(Math.random() * 4);
-				const speed = (this.size * this.velocity) * delta;
-				ctx.fillStyle = `hsl(${this.hue},90%,50%)`;
-				ctx.fillRect(this.x, this.y, this.size, this.size);
-				this.setDirection(randNum, speed);
-				this.x = Math.max(this.size, Math.min(width - this.size, this.x));
-				this.y = Math.max(this.size, Math.min(height - this.size, this.y));
-			}
+		x: number;
+		y: number;
+		r: number;
+		velocity: number;
+		size: number;
+		hue: number;
+		color: string;
+		angle: number;
+		radius: number;
+		cx: number;
+		cy: number;
+		constructor(size: number,hue:number) {
+			this.size = size;
+			this.r = Math.random();
+			this.x = Math.random() * width - this.size;
+			this.y = Math.random() * height - this.size;
+			this.velocity = Math.random() * 5
+			this.hue = hue
+			this.color = `hsl${this.hue},90%,50%`;
+			this.cx = width / 2;
+			this.cy = height / 2;
+			this.angle = Math.random() * Math.PI * 2;
+			this.radius = 5 + Math.random() * (width * 0.4);
+		}
+		show(delta: number) {
+			this.spin(delta);
+			ctx.fillStyle = `hsl(${this.hue},90%,50%)`;
+			ctx.fillRect(this.x, this.y, this.size, this.size);
+			// this.setRandDirection(delta);
+			//bounding to canvas
+			this.x = Math.max(this.size, Math.min(width - this.size, this.x));
+			this.y = Math.max(this.size, Math.min(height - this.size, this.y));
+		}
 
-			setDirection(randNum: number, speed: number) {
-				switch (randNum) {
-					case 0:
-						this.x = this.x + speed;
-						break;
-					case 1:
-						this.x = this.x - speed;
-						break;
-					case 2:
-						this.y = this.y + speed;
-						break;
-					case 3:
-						this.y = this.y - speed;
-						break;
-				}
-			}
+		spin(delta: number) {
+			this.angle += this.velocity * delta + 1;
 
-			checkBounds() {
-				//check left border
-				if (this.x + this.size <= 0) {
-					this.x = 0 + this.size;
-				}
-				//check right border
-				if (this.x - this.size >= width) {
-					this.x = width - this.size;
-				}
-				//check top
-				if (this.y + this.size <= 0) {
-					this.y = 0 + this.size;
-				}
-				//check bottom
-				if (this.y >= height - this.size) {
-					this.y = height - this.size;
-				}
+			this.x = this.cx + Math.cos(this.angle) * this.radius;
+			this.y = this.cy + Math.sin(this.angle) * this.radius;
+		}
+
+		setRandDirection(delta:number) {
+			const randNum = Math.floor(Math.random() * 4);
+			const speed = this.velocity * delta; 
+			switch (randNum) {
+				case 0:
+					this.x = this.x + 1 * speed;
+					break;
+				case 1:
+					this.x = this.x - 1 * speed;
+					break;
+				case 2:
+					this.y = this.y + 1 * speed;
+					break;
+				case 3:
+					this.y = this.y - 1 * speed;
+					break;
 			}
 		}
+	}
 
 	onMount(() => {
 		//get window dimensions
@@ -88,17 +86,19 @@
 		let lastTime = performance.now();
 
 		// svelte-ignore perf_avoid_nested_class
-		
 
 		let walkers = [];
+		const hue = Math.random() * 360
 		for (let index = 0; index < numberOfWalkers; index++) {
-			walkers.push(new Walker(Math.min(Math.random()*4,2)));
+			walkers.push(new Walker(Math.random()*3,hue+index));
 		}
 
 		function draw(time: number) {
 			const delta = (time - lastTime) / 1000;
 			lastTime = time;
 			// ctx?.clearRect(0, 0, width, height);
+			ctx.fillStyle = 'rgba(0,0,0,0.01)';
+			ctx.fillRect(0, 0, width, height);
 
 			for (const walker of walkers) {
 				walker.show(delta);
@@ -124,7 +124,6 @@
 <style>
 	canvas {
 		background-color: antiquewhite;
-		aspect-ratio: 1;
 		background-color: #010101;
 		display: flex;
 		flex-direction: column;
@@ -132,5 +131,7 @@
 		align-items: center;
 		border-radius: 4px;
 		box-shadow: hsl(240, 30%, 10%) 0 0 50px;
+		max-width: 90svw;
+		max-height: 90svh;
 	}
 </style>
